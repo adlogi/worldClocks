bf3413257a25a3dcf743722ec19b03a2fd6ad9a2
Abdulrahman Y. idlbi <adlogi@gmail.com>
2020-03-06 01:23:40 +0300

    the container for adding and updating clocks from different timezones

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Clock from './Clock';
+import AddClock from './AddClock';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    let now = new Date();
+    this.state = {
+      time: now,
+      timezones: [
+        {
+          id: 0,
+          name: Intl.DateTimeFormat().resolvedOptions().timeZone,
+        }
+      ],
+      lastId: 0,
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <AddClock handleNew={this.handleNew} />
+        {this.state.timezones.map(timezone => (
+          <Clock time={this.state.time} timezone={timezone} handleClose={this.handleClose} key={timezone.id} />
+        ))}
+      </div>
+    );
+  }
+
+  handleNew = (timezoneName) => {
+    const newTimezone = {
+      id: this.state.lastId + 1,
+      name: timezoneName,
+    }
+    this.setState(prevState => {
+      prevState.timezones.push(newTimezone);
+      return {
+        timezones: prevState.timezones,
+        lastId: prevState.lastId + 1,
+      }
+    })
+  }
+
+  handleClose = (id) =>{
+    let index = this.state.timezones.findIndex(timezone => timezone.id === id);
+    this.setState(prevState => {
+      prevState.timezones.splice(index, 1);
+      return prevState;
+    });
+  }
+
+  clockTick = () => {
+    this.setState({
+      time: new Date(),
+    });
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.clockTick, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
 }
-
-export default App;
